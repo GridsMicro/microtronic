27dd27ba0a9de1025f52e8a4d667c019ed9315c7
ShadowOfNaka <83060273+WebShardow@users.noreply.github.com>
2024-10-07 15:02:07 +0700

    update

--- update-version/microtronic.biz-v2/assets/js/main.js
+++ update-version/microtronic.biz-v2/assets/js/main.js
@@ -1,7 +1,4 @@
 /**
-* Template Name: BizLand
-* Template URL: https://bootstrapmade.com/bizland-bootstrap-business-template/
-* Updated: Aug 07 2024 with Bootstrap v5.3.3
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
